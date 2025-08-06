c4c4cc85af2b1874df9ad1c66e218c99a9c70160
Lokesh-Sutar <sutarlokesh82@gmail.com>
2025-08-07 01:37:50 +0530

    Updating

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,20 @@ import { isNil } from 'lodash';
 import { useHistory } from 'react-router';
 import { leaveRoom } from '../lib/endpoints';
 
+function Logo({ size = 25 }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 95 95"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+    <path d="M12 2C10.343 2 9 3.343 9 5v.535C6.72 6.482 5 8.602 5 11v5l-2 2v1h18v-1l-2-2v-5c0-2.398-1.72-4.518-4-5.465V5c0-1.657-1.343-3-3-3zm0 20c1.105 0 2-.895 2-2h-4c0 1.105.895 2 2 2z"/>
+  </svg>
+  );
+}
+
 export default function Header({
   auth = {},
   clearAuth,
@@ -29,7 +43,7 @@ export default function Header({
     <header>
       <Navbar>
         <Navbar.Brand>
-          BuzzerWeb
+          <Logo /> BuzzerWeb
         </Navbar.Brand>
         <div className="nav-buttons">
           {!isNil(sound) ? (
